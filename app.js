75226e61e3ba84fa701a16132d469552ea5e0ca6
Vsevolod-dev <60555096+Vsevolod-dev@users.noreply.github.com>
2021-05-23 22:18:07 +0300

    img

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
-//const request = require('request');
+const request = require('request');
 const express = require('express')
 const mongoose = require('mongoose');
+const json = require('json');
 
 // const Router = require('express-router')
 // const router = Router()
@@ -9,7 +10,6 @@ const mongoose = require('mongoose');
 //     console.log(ctx)
 //     ctx.status = 200
 // })
-
 
 const app = express() 
 
@@ -22,6 +22,12 @@ const bot = new TelegramBot(token, {
     // }
     polling: true
 });
+
+const now = new Date();
+const milliSeconds = now.getTime();
+const milliSeconds_plus_5min = milliSeconds + 300000
+const headers = {'Authorization': 'Bearer eyJrIjoiT0tTcG1pUlY2RnVKZTFVaDFsNFZXdE9ZWmNrMkZYbk'}
+
 
 async function start() {
     try {
@@ -62,6 +68,7 @@ async function start() {
             //         console.log(user)
             //     })
             //     .catch(e => console.log(e))
+            
             if (msg.text === '/start') {
             Users.find({chatId: chatId})
                 .then(users => {
@@ -79,9 +86,43 @@ async function start() {
                     bot.sendMessage(chatId, 'Ваш chatId = ' + msg.from.id + ', попросите администратора добавить вас в список, и продолжите работу с ботом');
                 })
             }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            if (msg.text === 'Аварии на данный момент') {
+                const url =  'http://exp-tools.unix.tensor.ru/queues/';
+                const r = request.get(url = url, verify=False);
+                const answ = r.json().incident_list[0];
 
-            if (msg.text !== '/start') {
-                bot.sendMessage(chatId, 'Выберите действие', {
+                bot.sendMessage(chatId, answ);
+            }
+
+            if (msg.text === 'Инциденты/триггеры') {
+                // const url =  `https://monitor.sbis.ru/d/b9DogIvGz/intsidenty?orgId=1&refresh=30s&from=${milliSeconds}&to=${milliSeconds_plus_5min}&viewPanel=4`;
+                // const r = request.get(url = url, headers = headers, verify=False);
+
+                // const exp_conf = request.json().panels.dutyInfo.properties.count.incident.exp[0];
+                // const spd_conf = request.json().panels.dutyInfo.properties.count.incident.spd[0];
+                // const sys_conf = request.json().panels.dutyInfo.properties.count.incident.sys[0];
+
+                // const exp_not_conf = request.json().panels.dutyInfo.properties.count.incident.exp[1];
+                // const spd_not_conf = request.json().panels.dutyInfo.properties.count.incident.spd[1];
+                // const sys_not_conf = request.json().panels.dutyInfo.properties.count.incident.sys[1];
+
+                bot.sendMessage(chatId, `
+                Отдел эксплуатации:
+Подтверждено: 11, Не подтверждено: 0
+Отдел СПД:
+Подтверждено: 13, Не подтверждено: 0
+Отдел администрирования:
+Подтверждено: 40, Не подтверждено: 0
+                `);
+            }
+
+            if (msg.text === 'Флаг аварии') {
+                bot.sendMessage(chatId, 'Напишите название аварии или сервиса');
+            }
+
+            if (msg.text === 'Мониторинг') {
+                bot.sendMessage(chatId, 'Выберите экран', {
                     reply_markup: {
                         keyboard: [
                             ['Ошибки облака', 'Очереди'],
@@ -90,6 +131,26 @@ async function start() {
                     }
                 });
             }
+
+            if (msg.text === 'Ошибки облака') {
+                // wget -O 1.svg `https://monitor.sbis.ru/d/000000125/pch-tsod-oshibki-oblaka?orgId=1&refresh=10s&from=${milliSeconds}&to=${milliSeconds_plus_5min}`
+                
+                // const img = 
+                bot.sendPhoto(chatId, './img/1');
+            }
+
+            if (msg.text === 'Очереди') {
+                bot.sendMessage(chatId, 'Очереди график');
+            }
+
+            if (msg.text === 'Контроль состояний виртуальных платформ') {
+                bot.sendMessage(chatId, 'Виртуальные платформы график');
+            }
+
+            if (msg.text === 'Контроль сервисов БД') {
+                bot.sendMessage(chatId, 'Контроль сервисов БД график');
+            }
+
           });
 
           bot.on("polling_error", (m) => console.log(m));
@@ -101,3 +162,20 @@ async function start() {
 
 start()
 
+// const request = require('request');
+// const json = require('json');
+
+// server = 'https://monitor.sbis.ru/d/Nx4A5nmmz/'
+
+// const url = server + 'api/dashboards/uid' + uid
+
+// https://monitor.sbis.ru/d/b9DogIvGz/intsidenty?orgId=1&refresh=30s&from=1621716327028&to=1621716627028
+
+// const headers = {'Authorization': 'Bearer eyJrIjoiT0tTcG1pUlY2RnVKZTFVaDFsNFZXdE9ZWmNrMkZYbk'}
+// curl -H "Authorization: Bearer eyJrIjoiYU9KZVFDaFMwakNIb3QzN1F0aTdFVUQ2ZjA3Qzh3eTAiLCJuIjoiZmlyc3RfdGVzdCIsImlkIjoxfQ==" http://34.71.93.17:3000/api/dashboards/home
+
+
+// const r = request.get(url = url, headers = headers, verify=True)
+// console.log(r.json())
+
+// console.log(dsa( 'google.com'))
